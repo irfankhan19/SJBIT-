1daec48c3adfdc53b019a9652934fff270cf7411
irfankhan19 <irfan122khan@gmail.com>
2014-02-15 19:02:56 +0530

    my first commit to 1.c

---
+++ 1.c
@@ -0,0 +1,3 @@
+ Notably, when you push to a remote repository, you do not have to push all of your branches. You can choose to share just one of your branches, a few of them, or all of them. This tends to free people to try new ideas without worrying about having to plan how and when they are going to merge it in or share it with others.
+
+There are ways to accomplish some of this with other systems, but the work involved is much more difficult and error-prone. Git makes this process incredibly easy and it changes the way most developers work when they learn it. 
